508eff696edd01f234657d506e94225afdbb220b
Siddhesh Deepak Hindalekar <v-sihin@microsoft.com>
2021-11-24 21:41:02 +0530

    Started Insights Automation

---
+++ js/common.js
@@ -0,0 +1,14 @@
+﻿function getBloggerData(blogUrl, successCallBack, completeCallBack) {
+    $.ajax({
+        url: blogUrl,
+        type: 'GET',
+        dataType: 'jsonp',
+        success: successCallBack,
+        complete: completeCallBack
+    });
+}
+
+function load_unload_modal(){
+    //document.getElementById("load-modal-header").show
+   // $('#contact-modal-header').toggleClass("hide");
+}
