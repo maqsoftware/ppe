8108d826bda3b48067f4cf2e97d9638e216f6bd9
Yash Shah <v-yassha@microsoft.com>
2021-11-26 19:52:48 +0530

    Fixed Send Mail

--- js/common.js
+++ js/common.js
@@ -12,3 +12,15 @@ function load_unload_modal(){
     //document.getElementById("load-modal-header").show
    // $('#contact-modal-header').toggleClass("hide");
 }
+
+function Sendmail() {
+    var str = $('#tml-content form').serialize().replace(/&/g, '%0D%0A').replace(/=/g, ': ');
+    var url = 'mailto:Sales@MAQSoftware.com?subject= Inquiry&body=' + str
+    document.location.href = url
+}
+
+function SendmailContact() {
+    var str = $('#contact-form').serialize().replace(/&/g, '%0D%0A').replace(/=/g, ': ');
+    var url = 'mailto:Sales@MAQSoftware.com?subject= Inquiry&body=' + str
+    document.location.href = url
+}
